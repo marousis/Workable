67b037c1d63554a3cca62312be81c7667ba77dde
Παναγιώτης Μαρούσης <marousis@gmail.com>
2021-12-13 11:53:24 +0200

    edit & delete task

--- cypress/integration/5_tasks.js
+++ cypress/integration/5_tasks.js
@@ -17,10 +17,10 @@ const description = "#description";
 const labels_selector = "#search_input";
 const upload_file = "#attachments";
 const create_btn = ".waves-effect";
-const view_tasks =
-  ":nth-child(1) > .row > .col > .card > .card-action > #btn_view_tasks";
-const first_todo_item = "#to_do_items > :nth-child(1)";
-const in_progress_column = "#in_progress_items";
+const edit_task_btn = "#btn_update_task";
+const edit_task_path = "update";
+const first_task_title = "#to_do_items > :nth-child(1) #card_title";
+const first_task_delete_btn = "#to_do_items > :nth-child(1) #btn_delete_task";
 
 //Data
 const email_valid = "marousis@gmail.com";
@@ -30,6 +30,9 @@ let random_task_summary = (Math.random() + 1).toString(36);
 let random_task_dercription = (Math.random() + 1).toString(36);
 const label = "backend";
 const filepath = "data.pdf";
+const edit_task_btn_text = "Edit";
+let random_task_edit_summary = (Math.random() + 1).toString(36);
+let random_task_edit_dercription = (Math.random() + 1).toString(36);
 
 context("Task tests", () => {
   before(() => {
@@ -51,25 +54,44 @@ context("Task tests", () => {
     // After eatch test preserve the local storage for the next test
     cy.saveLocalStorage();
   });
-  //   it("Creating a new task", () => {
-  //     // add negative assertions
-  //     cy.get(add_task_btn)
-  //       .should("be.visible")
-  //       .should("contain", add_task_btn_text)
-  //       .click();
-  //     cy.url().should("contain", new_task_path);
-  //     cy.get(summary).type(random_task_summary, { force: true });
-  //     cy.get(description).type(random_task_dercription, { force: true });
-  //     cy.get(labels_selector).type(label, { force: true });
-  //     cy.contains(label).click();
-  //     cy.get(upload_file).attachFile(filepath);
-  //     cy.get(create_btn).click();
-  //     cy.url().should("contain", tasks_path);
-  //   });
+  it("Creating a new task", () => {
+    // add negative assertions
+    cy.get(add_task_btn)
+      .should("be.visible")
+      .should("contain", add_task_btn_text)
+      .click();
+    cy.url().should("contain", new_task_path);
+    cy.get(summary).type(random_task_summary, { force: true });
+    cy.get(description).type(random_task_dercription, { force: true });
+    cy.get(labels_selector).type(label, { force: true });
+    cy.contains(label).click();
+    cy.get(upload_file).attachFile(filepath);
+    cy.get(create_btn).click();
+    cy.url().should("contain", tasks_path);
+  });
   it("Editing a task", () => {
-    //edit
+    // add negative assertions
+    cy.get(edit_task_btn)
+      .should("be.visible")
+      .should("contain", edit_task_btn_text)
+      .click();
+    cy.url().should("contain", edit_task_path);
+    cy.get(summary).clear().type(random_task_edit_summary, { force: true });
+    cy.get(description).type(random_task_edit_dercription, { force: true });
+    cy.get(create_btn).click();
+    cy.url().should("contain", tasks_path);
+    cy.contains(random_task_edit_summary).should("be.visible");
+    cy.contains(random_task_edit_dercription).should("be.visible");
   });
   it("Deleting a task", () => {
-    //delete
+    cy.get(first_task_title)
+      .invoke("text")
+
+      .then((value) => {
+        let title = value;
+
+        cy.get(first_task_delete_btn).click();
+        cy.contains(title).should("not.exist");
+      });
   });
 });
